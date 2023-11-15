c800767efcf56467bc6420afc88be32b47f50c6b
dev-JG <ljg615@nate.com>
2023-11-15 20:44:52 +0900

    git init

---
+++ src/main/java/com/category/CategoryApiApplication.java
@@ -0,0 +1,12 @@
+package com.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryApiApplication.class, args);
+    }
+}
